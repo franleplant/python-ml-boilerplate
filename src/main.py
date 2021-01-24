626cc6a0469ae5bd8eb520e95babd3819a2edb03
Fran Guijarro <franleplant@gmail.com>
2021-01-23 22:13:52 -0300

    plot some

--- src/main.py
+++ src/main.py
@@ -10,24 +10,25 @@ print(keras.__version__)
 fashion_mnist = keras.datasets.fashion_mnist
 (X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
 
-for i in range(4):
+for i in range(5):
     pyplot.subplot(330 + 1 + i)
-    pyplot.imshow(X_train[i + 100], cmap=pyplot.get_cmap("gray"))
-    pyplot.show()
+    pyplot.imshow(X_train_full[i + 100], cmap=pyplot.get_cmap("gray"))
 
-X_train = X_train_full[5000:] / 255.0
-X_valid = X_train_full[:5000] / 255.0
-y_train = y_train_full[5000:]
-y_valid = y_train_full[:5000]
+pyplot.show()
 
-X_test = X_test / 255.0
+# X_train = X_train_full[5000:] / 255.0
+# X_valid = X_train_full[:5000] / 255.0
+# y_train = y_train_full[5000:]
+# y_valid = y_train_full[:5000]
+
+# X_test = X_test / 255.0
 
 
-model = keras.model.Sequential(
-    [
-        layers.Flatten(input_shape=[28, 28]),
-        layers.Dense(300, activation="relu"),
-        layers.Dense(100, activation="relu"),
-        layers.Dense(10, activation="softmax"),
-    ]
-)
+# model = keras.model.Sequential(
+    # [
+        # layers.Flatten(input_shape=[28, 28]),
+        # layers.Dense(300, activation="relu"),
+        # layers.Dense(100, activation="relu"),
+        # layers.Dense(10, activation="softmax"),
+    # ]
+# )
