43e24ca60593f12ef5eaf407f0c13a2fb8aff4d0
Fran Guijarro <franleplant@gmail.com>
2021-01-23 22:08:14 -0300

    initial commit

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from matplotlib import pyplot
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+print(tf.__version__)
+
+print(keras.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+(X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
+
+for i in range(4):
+  pyplot.subplot(330 + 1 + i)
+  pyplot.imshow(X_train[i+100], cmap=pyplot.get_cmap('gray'))
+  pyplot.show()
+
+X_train  = X_train_full[5000:] / 255.0
+X_valid = X_train_full[:5000] / 255.0
+y_train = y_train_full[5000:]
+y_valid = y_train_full[:5000]
+
+X_test = X_test / 255.0
+
+
+model = keras.model.Sequential([
+  layers.Flatten(input_shape=[28,28]),
+  layers.Dense(300, activation="relu"),
+  layers.Dense(100, activation="relu"),
+  layers.Dense(10, activation="softmax"),
+])
